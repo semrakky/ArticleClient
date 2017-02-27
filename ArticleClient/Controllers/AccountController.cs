5405d476f40366f6279bf459cac2e7ba3c7a815a
CHHUNLY <chhunly.smart@gmail.com>
2017-02-27 16:23:21 +0700

    add account controller

---
+++ ArticleClient/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ArticleClient.Models;
+namespace ArticleClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(string username, string password){
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:55550/api/");
+
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var responseTask = httpClient.GetAsync("User/GetSession?name="+username+"&password="+password);
+            responseTask.Wait();
+
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                System.Diagnostics.Debug.WriteLine("Something...username="+username);
+                System.Diagnostics.Debug.WriteLine("Something...password=" + password);
+                System.Diagnostics.Debug.WriteLine("status code="+result.IsSuccessStatusCode);
+               
+                var readTask = result.Content.ReadAsAsync<usersession>();
+                
+                readTask.Wait();
+                
+                Session["user"] = readTask.Result;
+                return Redirect("../Home/");
+            }
+
+            return Redirect("../Account");
+        }
+
+	}
+}
---
+++ ArticleClient/Models/usersession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArticleClient.Models
+{
+    public class usersession
+    {
+        public short Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Gender { get; set; }
+
+        public string Passwd { get; set; }
+        public String RoleName { get; set; }
+        public String RoleDescription { get; set; }
+    }
+}
